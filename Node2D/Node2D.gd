d3b881f41b8bb0730c66ade7803ade4f396fcdd7
sgabrams <59935929+sgabrams@users.noreply.github.com>
2020-09-10 21:22:54 -0400

    finished 2d node 2

--- Node2D/Node2D.gd
+++ Node2D/Node2D.gd
@@ -8,7 +8,8 @@ var scale_steps = 0
 onready var global = get_node("/root/Global")
 
 func translating(px,py,amount):
-	pass
+	 position.x = sin(px)*amount
+  	 position.y = sin(py)*amount
 
 func rotating(rad):
    rotation = sin(rot_steps)
@@ -18,17 +19,14 @@ func scaling(amount):
    scale.y = sin(amount) + 1.1
 	pass
 
-
 func _physics_process(delta):
+	if global.won:
+		return
+	translating(posx_steps,posy_steps,600)
+	rotating(rot_steps)
+	scaling(scale_steps)
 	posx_steps += 0.007
    	posy_steps += 0.011
 	scale_steps += 0.003
 	rot_steps += 0.005
-	if global.won:
-		return
-func translating(px,py,amount):
-   position.x = sin(px)*amount
-   position.y = sin(py)*amount
-	rotating(rot_steps)
-	scaling(scale_steps)
-
+	
